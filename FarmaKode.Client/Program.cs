99ef7dfc48acc2bffae198d42da258972b340735
Ahmet YAĞBASAN <ayagbasan@eximbank.gov.tr>
2020-03-28 17:16:27 +0300

    fix

--- FarmaKode.Client/Program.cs
+++ FarmaKode.Client/Program.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using DevExpress.UserSkins;
-using DevExpress.Skins;
-using DevExpress.LookAndFeel;
 using FarmaKode.Client.Business;
 
 namespace FarmaKode.Client
