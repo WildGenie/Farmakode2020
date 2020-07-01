f627b4d75309201b060c3d2701b223b70a35b7e2
Ahmet YAĞBASAN <ahmetyagibasan@gmail.com>
2020-07-02 01:20:19 +0300

    run as admin

--- FarmaKode.Client/Program.cs
+++ FarmaKode.Client/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.Windows.Forms;
 using FarmaKode.Client.Business;
 using FarmaKode.Client.Model.Section;
@@ -25,9 +28,47 @@ namespace FarmaKode.Client
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-           
 
-            Application.Run(new FormButton());
+            var wi = WindowsIdentity.GetCurrent();
+            var wp = new WindowsPrincipal(wi);
+
+            bool runAsAdmin = wp.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!runAsAdmin)
+            {
+                // It is not possible to launch a ClickOnce app as administrator directly,
+                // so instead we launch the app as administrator in a new process.
+                var processInfo = new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase);
+
+                // The following properties run the new process as administrator
+                processInfo.UseShellExecute = true;
+                processInfo.Verb = "runas";
+
+                // Start the new process
+                try
+                {
+                    Process.Start(processInfo);
+                }
+                catch (Exception)
+                {
+                    // The user did not allow the application to run as administrator
+                    MessageBox.Show("Sorry, but I don't seem to be able to start this program with administrator rights!");
+                }
+
+                // Shut down the current process
+                Application.Exit();
+            }
+            else
+            {
+                // We are running as administrator
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new FormButton());
+            }
+
+
+
+           
         }
 
 
